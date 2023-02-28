20baa3af2fbf35ee0e4b4686f503e3429bf3f1a7
Frank Patrick Namegni <CodeShadowing95>
2023-02-28 10:33:37 +0100

    SongDetails, DetailsHeader, RelatedSongs modules edited

--- src/redux/services/shazam-core.js
+++ src/redux/services/shazam-core.js
@@ -12,7 +12,13 @@ export const shazamCoreAPI = createApi({
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => '/charts/world' }),
+    getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
+    getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
   }),
 });
 
-export const { useGetTopChartsQuery } = shazamCoreAPI;
+export const {
+  useGetTopChartsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+} = shazamCoreAPI;
